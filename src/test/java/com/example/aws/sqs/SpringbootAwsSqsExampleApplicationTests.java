0d97a2049aeda885697ea445a596b007d727039a
venkatRaaa <venkat129rv4@gmail.com>
2021-01-08 19:26:31 -0600

    initial commit

---
+++ src/test/java/com/example/aws/sqs/SpringbootAwsSqsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.aws.sqs;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringbootAwsSqsExampleApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
